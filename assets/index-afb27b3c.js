b67a2f9630edc235e7cf2c5a073b7b5022b08e12
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-09-27 03:08:33 +0000

    Deploying to gh-pages from @ SergiyVitruk/Scss-learning@1eac1db1fb8c77fa5b836d9a836290d35a801097 🚀

--- assets/index-afb27b3c.js
+++ assets/index-afb27b3c.js
@@ -39,4 +39,34 @@
     fetch(link.href, fetchOpts);
   }
 })();
-const style = "";
+const main = "";
+if (typeof window !== "undefined") {
+  var passiveTestOptions = {
+    get passive() {
+      return void 0;
+    }
+  };
+  window.addEventListener("testPassive", null, passiveTestOptions);
+  window.removeEventListener("testPassive", null, passiveTestOptions);
+}
+const refsMenu = {
+  openMenuBtn: document.querySelector("js-menu-open"),
+  closeMenuBtn: document.querySelector("js-menu-close"),
+  overlayMenu: document.querySelector("js-menu")
+};
+const toggleMenu = () => {
+  const isMenuOpen = refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  refsMenu.overlayMenu.classList.toggle("is-open");
+  const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
+  bodyScrollLock[scrollLockMethod](document.body);
+};
+refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
+refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
+window.matchMedia("(min-width: 1200px").addEventListener("change", (event) => {
+  if (!event.matches)
+    return;
+  refsMenu.overlayMenu.classList.remove("is-open");
+  refsMenu.openMenuBtn.setAttribute("aria-expended", false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
